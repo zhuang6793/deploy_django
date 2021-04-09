e15d43ddde1ed2ca8abc8293297eeed9d35e5edf
zhuang6793 <444764906@qq.com>
2021-04-09 18:39:12 +0800

    update html

--- my_deploy/my_deploy/settings.py
+++ my_deploy/my_deploy/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         'NAME': 'oam_db',
         'USER': 'root',
         'PASSWORD': 'admin@123',
-        # 'HOST': '192.168.1.65',
-        'HOST': '127.0.0.1',
+        'HOST': '192.168.1.65',
+        # 'HOST': '127.0.0.1',
         'PORT': '3306',
 
     }
@@ -127,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'deploy_app/static')
